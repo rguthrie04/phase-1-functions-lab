51ab3f25c0e47ce07e03f2897f47845d4b9b7086
Ryan Guthrie <ryanguthrie@zoho.com>
2021-08-10 17:56:51 +0100

    commit tree c3de14809c5ddd0618730d3003ea11a0c9fc3f60

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const ourBlock = 42
+
+function distanceFromHqInBlocks(theirBlock){
+        if (theirBlock > ourBlock) { 
+            return theirBlock - ourBlock
+        } else  {
+            return ourBlock - theirBlock
+}}
+
+const blockFeet = 264
+const rate1 = .02
+const flatFee = 25
+const freeFeet = 400
+
+function distanceFromHqInFeet(theirFeet) {
+    return distanceFromHqInBlocks(theirFeet) * blockFeet
+}
+
+function distanceTravelledInFeet(startBlock, finishBlock){
+    return (distanceFromHqInFeet(finishBlock) - distanceFromHqInFeet(startBlock))
+}
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination)
+    if (distance < freeFeet){
+        return 0;
+    } else if (distance>freeFeet && distance<2000){
+        return (distance - freeFeet) * rate1;
+    } else if (distance>2000 && distance<2500){
+            return flatFee
+    } else if (distance>2500) {
+        return 'cannot travel that far'
+    }
+
+    }
